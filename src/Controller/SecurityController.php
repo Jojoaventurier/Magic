60d10aa59c1741395fe0da3ab90802ab1c7df8e0
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-07-25 17:04:13 +0200

    updt

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    public const SCOPES = [
+        'google' => [],
+    ];
+
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -23,10 +31,49 @@ class SecurityController extends AbstractController
             'error' => $error,
         ]);
     }
+    
+
+
+
+    #[Route(path: "/googlelogin", name: 'oauth_login_google', methods: ['GET'])]
+    public function oauthlogin(AuthenticationUtils $authenticationUtils): Response {
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute(route:'app_home');
+        }
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render("security/login.html.twig", [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
+
+    }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route("/oauth/connect/{service}", name: 'auth_oauth_connect', methods: ['GET'])]
+    public function connect(string $service, ClientRegistry $clientRegistry): RedirectResponse
+    {
+       if (! in_array($service, array_keys(array: self::SCOPES), strict: true)) {
+           throw $this->createNotFoundException();
+       }
+
+       return $clientRegistry
+           ->getClient($service)
+           ->redirect(self::SCOPES[$service]);
+    }
+
+    #[Route('/oauth/check/{service}', name: 'auth_oauth_check', methods: ['GET', 'POST'])]
+    public function check(): Response
+    {
+       return new Response(status: 200);
+    }
+
+    
 }
---
+++ src/Security/OAuthRegistrationService.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use League\OAuth2\Client\Provider\GoogleUser;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+
+final readonly class OAuthRegistrationService
+{
+
+    public function __construct(
+        private UserRepository $repository
+    ) {
+
+    }
+
+    /**
+     * @param GoogleUser $resourceOwner 
+     */
+
+
+public function persist(ResourceOwnerInterface $resourceOwner): User
+{
+    $currentDateTime = new \DateTime('now');
+    $user = (new User())
+    ->setEmail($resourceOwner->getEmail())
+    ->setGoogleId($resourceOwner->getId())
+    ->setCreationDate($currentDateTime)
+    ->setUserName($resourceOwner->getEmail());
+
+     $this->repository->add($user, flush: true);
+     return $user;
+
+      //  $user = match (true) {
+    //     $resourceOwner instanceof GoogleUser => (new User())
+    //     ->setEmail($resourceOwner->getEmail())
+    //     ->setGoogleId($resourceOwner->getId()),
+    //     $resourceOwner instanceof GithubResourceOwner => (new User())
+    //     ->setEmail($resourceOwner->getEmail()
+    //     ->setGithubId($resourceOwner->getId()))
+    //  };
+
+
+}
+
+
+}
