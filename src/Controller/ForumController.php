4b4853124f22ffbf43c6cf55fef940b1ba4df1d3
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-26 17:12:33 +0200

    continued trhe forum section

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\ForumCategory;
+use App\Entity\ForumSubCategory;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ForumCategoryRepository;
+use App\Repository\ForumSubCategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ForumController extends AbstractController
 {   
     #[IsGranted('ROLE_USER')]
     #[Route('/forum', name: 'app_forum')]
-    public function index(): Response
+    public function index(ForumCategoryRepository $categoryRepository, ForumSubCategoryRepository $subCategoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+        $subCategories = $subCategoryRepository->findAll();
+
+
         return $this->render('forum/index.html.twig', [
-            'controller_name' => 'ForumController',
+            'categories' => $categories,
+            'subCategories' => $subCategories
         ]);
     }
 }
