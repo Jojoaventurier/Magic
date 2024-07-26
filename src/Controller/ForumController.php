e5480418f3ee2ceaebe921b889e6953a7966e4c7
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-26 16:35:00 +0200

    updt

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -7,7 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ForumController extends AbstractController
-{
+{   
+    #[IsGranted('ROLE_USER')]
     #[Route('/forum', name: 'app_forum')]
     public function index(): Response
     {
