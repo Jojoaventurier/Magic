9a2dab74de4dc5a97591221d63347b26e70d889d
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-26 09:55:17 +0200

    created article and category entity

---
+++ src/Entity/Article.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $articleTitle = null;
+
+    #[ORM\Column(length: 10000)]
+    private ?string $articleText = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $creationDate = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArticleTitle(): ?string
+    {
+        return $this->articleTitle;
+    }
+
+    public function setArticleTitle(string $articleTitle): static
+    {
+        $this->articleTitle = $articleTitle;
+
+        return $this;
+    }
+
+    public function getArticleText(): ?string
+    {
+        return $this->articleText;
+    }
+
+    public function setArticleText(string $articleText): static
+    {
+        $this->articleText = $articleText;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): static
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
